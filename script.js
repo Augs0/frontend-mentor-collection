ed9dd20c6c5af654a575a230ba2da8a253ce3945
August Aldred <augustaldred@bpp.com>
2020-08-20 09:23:24 +0100

    added latest project to homepage

--- script.js
+++ script.js
@@ -8,25 +8,32 @@ const feProjects = [
         name: "huddle-landing-page",
         friendly: "Huddle Landing Page",
         used: "HTML, SCSS, Gulp 4"
+    },
+    {
+        name: "article-preview",
+        friendly: "Article Preview",
+        used: "HTML, CSS, Javascript"
     }
 ]
 
 const list = document.querySelector('.list');
 
-feProjects.forEach(({ name, friendly, used }, project) => {
+feProjects.forEach(({ name, friendly, used }) => {
     const item = document.createElement('LI');
 
     item.innerHTML = `
-    <li>
+    
     <div class="img-wrapper">
-    <img src="/${name}/starter files/design/desktop-design.jpg" alt="${name}" /></div>
+    <img src="/${name}/starter_files/design/desktop-design.jpg" alt="${name}" />
+    </div>
+    <div class="text-wrapper">
         <a href="/${name}/index.html" aria-label="view ${friendly} project">
         <p class="subtitle">Project: </p>
         </a>
         <p>${friendly}</p>
         <p class="subtitle">Tech used: </p>
         <p>${used}</p>
-        </li>
+        </div>
     `
 
     list.appendChild(item);
